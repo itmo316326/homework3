6120fd0e4cd2e9ce6f0450b9cdd9caa63fe2e7f7
itmo316326 <91429894+itmo316326@users.noreply.github.com>
2022-03-30 20:21:59 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,13 +1,22 @@
+import secrets
 from flask import Flask, request, render_template, redirect, session, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
+secret = secrets.token_urlsafe(32)
+app.secret_key = secret
 
 client = MongoClient('localhost', 27017)
 db = client.wad
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
+def index():
+    all_post = db.users.find({"post": "post"})
+    return render_template("index.html", all_post=all_post)
+
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -15,40 +24,58 @@ def login():
         is_user_online = db.users.find_one({"username": username, "password": password})
         if is_user_online:
             session['username'] = username
-            return redirect(url_for("http://localhost:5000/profile"))
+            return redirect(url_for("profile"))
         else:
             message = "Failed Login"
-            return render_template('login.html', message=message)
-    return render_template('login.html')
+            return render_template("login.html", message=message)
+    return render_template("login.html")
 
 
-@app.route("/registered/", methods=['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        user_is_online = db.insert_one({"username": username, "password": password})
+        user_is_online = db.users.insert_many([{"username": username, "password": password} for _ in range(1)])
         if user_is_online:
             session['username'] = username
-            return redirect(url_for("http://localhost:5000/profile"))
+            return redirect(url_for("profile"))
         else:
             message = 'User not registered'
-            return render_template('register.html', message=message)
-    return render_template('register.html')
+            return render_template("register.html", message=message)
+    return render_template("register.html")
 
 
 @app.route("/profile")
 def profile():
     if session.get("username"):
-        return redirect(url_for("http://localhost:5000/profile/"))
+        post_list = refresh_information()
+        return render_template("profile.html", post_list=post_list)
     else:
-        return render_template('login.html')
-    return render_template('profile.html')
+        return render_template("login.html")
 
 
+@app.route("/logout")
 def log_out():
     session.clear()
-    return redirect('login.html')
+    return redirect(url_for("login"))
+
+
+@app.route("/new", methods=['GET', 'POST'])
+def creat_new_post():
+    if request.method == 'POST':
+        post_title = request.form["p_title"]
+        post_content = request.form["p_content"]
+        post_type = request.form["p_type"]
+        is_post_success = db.users.insert_many([{"post": "post", "title": post_title, "content": post_content, "type": post_type,"username": session.get("username")} for _ in range(1)])
+        if is_post_success:
+            return redirect(url_for("profile"))
+    return render_template("new.html")
+
+
+def refresh_information():
+    post_list = db.users.find({"post": "post", "username": session.get("username")})
+    return post_list
 
 
 if __name__ == '__main__':
